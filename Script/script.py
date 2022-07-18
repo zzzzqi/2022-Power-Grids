de0a8a9453839905241dad94b8cff2a526ff792e
lhh1999 <1052619713@qq.com>
2022-07-18 11:41:22 +0100

    add the script_2.py

--- Script/script.py
+++ Script/script.py
@@ -69,18 +69,18 @@ psr_dir = current_dir + os.sep + 'prediction_data'
 mkdir(psr_dir)
 csv_suffix = '.csv$'
 
-df_columns_predictions = ["event_id", "start_time", "asset_name", "manual_event_type", "input_event_csv_filename", 
+df_columns_predictions = ["event_id", "start_time", "asset_name", "manual_event_type", "input_event_csv_filename",
             "vab_flickers", "vab_harmonics", "vab_interruptions", "vab_interruptions_harmonics", "vab_osc_transients",
-            "vab_sags", "vab_sags_harmonics", "vab_spikes", "vab_swells", "vab_swells_harmonics", "vbc_flickers", 
-            "vbc_harmonics", "vbc_interruptions", "vbc_interruptions_harmonics", "vbc_osc_transients", "vbc_sags", 
-            "vbc_sags_harmonics", "vbc_spikes", "vbc_swells", "vbc_swells_harmonics", "vca_flickers", 
-            "vca_harmonics", "vca_interruptions", "vca_interruptions_harmonics", "vca_osc_transients", 
-            "vca_sags", "vca_sags_harmonics", "vca_spikes", "vca_swells", "vca_swells_harmonics", "ia_flickers", 
-            "ia_harmonics", "ia_interruptions", "ia_interruptions_harmonics", "ia_osc_transients", "ia_sags", 
-            "ia_sags_harmonics", "ia_spikes", "ia_swells", "ia_swells_harmonics", "ib_flickers", "ib_harmonics", 
-            "ib_interruptions", "ib_interruptions_harmonics", "ib_osc_transients", "ib_sags", "ib_sags_harmonics", 
-            "ib_spikes", "ib_swells", "ib_swells_harmonics", "ic_flickers", "ic_harmonics", "ic_interruptions", 
-            "ic_interruptions_harmonics", "ic_osc_transients", "ic_sags", "ic_sags_harmonics", "ic_spikes", 
+            "vab_sags", "vab_sags_harmonics", "vab_spikes", "vab_swells", "vab_swells_harmonics", "vbc_flickers",
+            "vbc_harmonics", "vbc_interruptions", "vbc_interruptions_harmonics", "vbc_osc_transients", "vbc_sags",
+            "vbc_sags_harmonics", "vbc_spikes", "vbc_swells", "vbc_swells_harmonics", "vca_flickers",
+            "vca_harmonics", "vca_interruptions", "vca_interruptions_harmonics", "vca_osc_transients",
+            "vca_sags", "vca_sags_harmonics", "vca_spikes", "vca_swells", "vca_swells_harmonics", "ia_flickers",
+            "ia_harmonics", "ia_interruptions", "ia_interruptions_harmonics", "ia_osc_transients", "ia_sags",
+            "ia_sags_harmonics", "ia_spikes", "ia_swells", "ia_swells_harmonics", "ib_flickers", "ib_harmonics",
+            "ib_interruptions", "ib_interruptions_harmonics", "ib_osc_transients", "ib_sags", "ib_sags_harmonics",
+            "ib_spikes", "ib_swells", "ib_swells_harmonics", "ic_flickers", "ic_harmonics", "ic_interruptions",
+            "ic_interruptions_harmonics", "ic_osc_transients", "ic_sags", "ic_sags_harmonics", "ic_spikes",
             "ic_swells", "ic_swells_harmonics"]
 
 f = open('output.csv', 'w')
@@ -109,57 +109,57 @@ f.close()
 
 
 
-#
-# ### Feed images into CNN model for scores ###
-#
-# # Load the trained CNN model
-# model_name = "pqd_cnn_test01_dataset05_model.h5"
-# # model_path = current_directory + os.sep + model_name
-# model_path = current_dir + os.sep + "trained_models" + os.sep + model_name
-# cnn = models.load_model(model_path)
-#
-# # # Define a pd dataframe
-# # df_columns = ["image_name", "wave", "flickers", "harmonics", "interruptions", "interruptions_harmonics",
-# #                 "osc_transients", "sags", "sags_harmonics", "spikes", "swells", "swells_harmonics"]
-#
-# # df = pd.DataFrame(columns=df_columns)
-#
-# # Import the prediction dataset
-# prediction_set_path = current_dir + os.sep + "prediction_data"
-# prediction_set = tf.io.gfile.listdir(prediction_set_path)
-#
-# # Iterate through the dataset, and make predictions with the trained CNN model.
-# # Log the prediction scores in the pd dataframe
-#
-# col_names = ["flickers", "harmonics", "interruptions", "interruptions_harmonics",
+
+### Feed images into CNN model for scores ###
+
+# Load the trained CNN model
+model_name = "pqd_cnn_test01_dataset05_model.h5"
+# model_path = current_directory + os.sep + model_name
+model_path = current_dir + os.sep + "trained_models" + os.sep + model_name
+cnn = models.load_model(model_path)
+
+# # Define a pd dataframe
+# df_columns = ["image_name", "wave", "flickers", "harmonics", "interruptions", "interruptions_harmonics",
 #                 "osc_transients", "sags", "sags_harmonics", "spikes", "swells", "swells_harmonics"]
-# count = 0
-# output_file = pd.read_csv('output.csv', index_col=4)
-# for image_name in prediction_set:
-#     if re.search(".png$", image_name) is not None:
-#         wave = image_name[:len(image_name)-4].split("_")[-1]
-#         input_event_csv_filename = image_name[:len(image_name)-4].rsplit("_", 1)[0]
-#
-#         prediction_image = preprocessing.image.load_img(
-#             path=prediction_set_path + "/" + image_name,
-#             color_mode='grayscale',
-#             target_size=(200, 200)
-#         )
-#         prediction_image_array = preprocessing.image.img_to_array(
-#             prediction_image
-#         )
-#         prediction_image_array = np.array(
-#             [prediction_image_array]
-#         )
-#         predictions = cnn.predict(prediction_image_array)
-#         for i in range(10):
-#             output_file.loc[input_event_csv_filename, wave.lower()+'_'+col_names[i]] = predictions[0][i]            ### Use input_event_csv_filname as the index to identify which cell to go into
-#         count += 1
-#
-# # change the positions of columns
-# # output_file.reset_index(inplace=True) # option 'inplace' means keep the change
-# # output_file = output_file.reindex(columns=df_columns_predictions)
-#
-#
-# # Save the pd dataframe as a CSV file
-# output_file.to_csv(current_dir + os.sep + "output.csv")
+
+# df = pd.DataFrame(columns=df_columns)
+
+# Import the prediction dataset
+prediction_set_path = current_dir + os.sep + "prediction_data"
+prediction_set = tf.io.gfile.listdir(prediction_set_path)
+
+# Iterate through the dataset, and make predictions with the trained CNN model.
+# Log the prediction scores in the pd dataframe
+
+col_names = ["flickers", "harmonics", "interruptions", "interruptions_harmonics",
+                "osc_transients", "sags", "sags_harmonics", "spikes", "swells", "swells_harmonics"]
+count = 0
+output_file = pd.read_csv('output.csv', index_col=4)
+for image_name in prediction_set:
+    if re.search(".png$", image_name) is not None:
+        wave = image_name[:len(image_name)-4].split("_")[-1]
+        input_event_csv_filename = image_name[:len(image_name)-4].rsplit("_", 1)[0]
+
+        prediction_image = preprocessing.image.load_img(
+            path=prediction_set_path + "/" + image_name,
+            color_mode='grayscale',
+            target_size=(200, 200)
+        )
+        prediction_image_array = preprocessing.image.img_to_array(
+            prediction_image
+        )
+        prediction_image_array = np.array(
+            [prediction_image_array]
+        )
+        predictions = cnn.predict(prediction_image_array)
+        for i in range(10):
+            output_file.loc[input_event_csv_filename, wave.lower()+'_'+col_names[i]] = predictions[0][i]            ### Use input_event_csv_filname as the index to identify which cell to go into
+        count += 1
+
+# change the positions of columns
+# output_file.reset_index(inplace=True) # option 'inplace' means keep the change
+# output_file = output_file.reindex(columns=df_columns_predictions)
+
+
+# Save the pd dataframe as a CSV file
+output_file.to_csv(current_dir + os.sep + "output.csv")
